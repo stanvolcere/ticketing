a43b809c3f2de568d89df6b6e9b451d34c339c63
stanvolcere <stanvolcere@hotmail.com>
2020-09-17 00:16:28 +0100

    added signout route handler

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,23 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
 router.get('/api/users/currentuser', (req, res) => {
-    res.send('New changes from current user AGAIN AGAIN 2');
+    // equivalent to - if (!req.session || !req.session.jwt)
+    if (!req.session?.jwt) {
+        return res.send({ currentuser: null });
+    }
+
+    try {
+        // process.env.JWT_KEY! tells typescript that we are sure that this exists
+        const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+
+        // tells the frontend app that thw user is currently logged in
+        res.send({ currentUser: payload });
+    } catch (err) {
+        res.send({ currentUser: null });
+    }
 });
 
 export { router as currentUserRouter };
--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -3,7 +3,10 @@ import express from 'express';
 const router = express.Router();
 
 router.post('/api/users/signout', (req, res) => {
-    res.send('hi there son');
+    // we essentially dump the cookie session which means 
+    // our JWT stored on there will be destroyed
+    req.session = null;
+    res.send({});
 });
 
 export { router as signoutRouter };
