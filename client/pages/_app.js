5b07a4fcec28cc9efafdf660c54e398b5259b810
stanvolcere <stanvolcere@hotmail.com>
2020-09-24 23:21:13 +0100

    added signup page + global css

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,6 +1,6 @@
-import 'bootstarp/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 // any css that we need imported throughout our app needs to be added here
 export default ({ Component, pageProps }) => {
-    return <Component {...pageProps} />
+    return <Component {...pageProps} />;
 }
