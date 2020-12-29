18d674fb111c43cd1931d8f6b0262b466a1f3322
stanvolcere <stanvolcere@hotmail.com>
2020-12-29 23:03:25 +0400

    update test setup for authentication

--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -2,53 +2,62 @@ import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
 import request from 'supertest';
 import { app } from '../app';
+import jwt from "jsonwebtoken"
 
 declare global {
-    namespace NodeJS {
-        interface Global {
-            signin(): Promise<string[]>
-        }
-    }
+	namespace NodeJS {
+		interface Global {
+			signin(): string[]
+		}
+	}
 }
 
 let mongo: any;
 
 beforeAll(async () => {
-    process.env.JWT_KEY = "echoes"
+	process.env.JWT_KEY = "echoes"
 
-    mongo = new MongoMemoryServer();
-    const mongoUri = await mongo.getUri();
+	mongo = new MongoMemoryServer();
+	const mongoUri = await mongo.getUri();
 
-    await mongoose.connect(mongoUri, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    });
+	await mongoose.connect(mongoUri, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true
+	});
 });
 
 beforeEach(async () => {
-    const collections = await mongoose.connection.db.collections();
+	const collections = await mongoose.connection.db.collections();
 
-    for (let collection of collections) {
-        await collection.deleteMany({});
-    }
+	for (let collection of collections) {
+		await collection.deleteMany({});
+	}
 });
 
 afterAll(async () => {
-    await mongo.stop();
-    await mongoose.connection.close();
+	await mongo.stop();
+	await mongoose.connection.close();
 });
 
-global.signin = async () => {
-    const email = "test@test.com";
-    const password = "password";
+global.signin = () => {
+	const payload = {
+		id: "qwerty",
+		email: "test@test.com",
+	}
 
-    const response = await request(app)
-        .post('/api/users/signup')
-        .send({
-            email, password
-        })
-        .expect(201);
+	// ! tells typescript to not worry about process.env.JWT_KEY  
+	// potentially being undefined because we are almost certain that 
+	// it is defined
+	const token = jwt.sign(payload, process.env.JWT_KEY!);
 
-    const cookie = response.get('Set-Cookie');
-    return cookie;
+	const session = { jwt: token }
+
+	const sessionJSON = JSON.stringify(session)
+
+	const base64 = Buffer.from(sessionJSON).toString('base64');
+
+	console.log(base64)
+
+	// returned as an [] to be compatible with what supertest is expecting
+	return [`express:sess=${base64}`];
 }
