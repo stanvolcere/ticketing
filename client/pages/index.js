67f3f0dbe3ad7ffdee0dc2543186592ad22e960e
Stan <stanvolcere@hotmail.com>
2022-03-03 20:54:01 +0000

    Update index.js

--- client/pages/index.js
+++ client/pages/index.js
@@ -14,23 +14,3 @@ HomePage.getInitialProps = async (context) => {
 }
 
 export default HomePage
-
-// <RespondToQuestions questionset={questionset} questions={questions} responses={re}></RespondToQuestions>
-
-// try {
-//     const questionset_res = await axios.get(`http://localhost:5000/questionsets/${questionset_id}?links=false`);
-//     const questionset = questionset_res.data.data;
-
-//     const questions_res = await axios.get(`http://localhost:5000/questionsets/${questionset_id}/questions?links=false`);
-//     const questions = questions_res.data.data;
-
-//     const responses_res = await axios.get(`http://localhost:5000/responsesets?links=false&questionset_id=${questionset_id}`).catch((err) => {
-//         console.log(err);
-//         return [];
-//     });
-//     const responses = responses_res.data.data;
-
-//     return { questionset, questions, responses };
-// } catch(err) {
-//     console.log();
-// }
